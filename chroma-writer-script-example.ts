07572505a5432424e81403066fb2a93f90cc88fb
Jeromephilip <jeromephilip03@gmail.com>
2024-10-06 17:48:25 -0400

    populating db with openai embeddings

--- chroma-writer-script-example.ts
+++
@@ -1,9 +0,0 @@
-import { getCourseDataForChroma } from "./course-document-fetcher";
-
-// Example of using the course document fetcher script to get the course data needed for chromadb document.
-try {
-    const chromaData = await getCourseDataForChroma();
-    console.log(chromaData);
-} catch (error) {
-    console.error('There was an error running the fetching script: ', error);
-}
